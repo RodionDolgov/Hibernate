f2e0db01be306fda4d4f51305c91d8733c3b5323
Dolgov <dolgovrodion@gmail.com>
2025-05-04 23:46:44 +0300

    Base operations

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -4,9 +4,7 @@ import org.example.models.Person;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
-/**
- * Hello world!
- */
+
 public class App {
     public static void main(String[] args) {
         Configuration configuration = new Configuration().addAnnotatedClass(Person.class);
@@ -17,8 +15,14 @@ public class App {
         try {
             session.beginTransaction();
 
+            //create
             Person person = new Person("Bob", 85);
             session.save(person);
+            //update
+            Person person2 = session.get(Person.class, 2);
+            person2.setName("Changed Name");
+            //delete
+            session.delete(person);
 
             session.getTransaction().commit();
             System.out.println(person.getId());
