5b2be90c15a70fb905018c57ff51377f5a51fafa
Dolgov <dolgovrodion@gmail.com>
2025-05-15 12:49:23 +0300

    1) Added Entity Item 2) Do CRUD op in main

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,15 +1,19 @@
 package org.example;
 
+import org.example.models.Item;
 import org.example.models.Person;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
+import java.util.Optional;
 
 public class App {
     public static void main(String[] args) {
-        Configuration configuration = new Configuration().addAnnotatedClass(Person.class);
+        Configuration configuration = new Configuration().addAnnotatedClass(Person.class).addAnnotatedClass(Item.class);
 
         SessionFactory sessionFactory = configuration.buildSessionFactory();
         Session session = sessionFactory.getCurrentSession();
@@ -17,17 +21,50 @@ public class App {
         try {
             session.beginTransaction();
 
-            //update, read, delete
-            session.createQuery("update Person set name = 'newName' where age = 23").executeUpdate();
-            List<Person> personByAge = session.createQuery("from Person where age > 55").getResultList();
-            for(Person person: personByAge) {
-                System.out.println(person);
+            //Get items
+            Person person = session.get(Person.class, 1);
+            System.out.println(person);
+            List<Item> items = person.getItems();
+            for (Item item : items) {
+                System.out.println(item.getItemName());
             }
-            List<Person> personByName = session.createQuery("from Person where name like 'W%'").getResultList();
-            for(Person person: personByName) {
-                System.out.println(person);
+
+            //Get owner
+            Item item = session.get(Item.class, 3);
+            System.out.println(item.getOwner());
+            //Add new Item
+            Person secondPerson = session.get(Person.class, 2);
+            Item newItem = new Item("Chair", secondPerson);
+            secondPerson.getItems().add(newItem);
+            session.save(newItem);
+            //Add new Person
+            Person newPerson = new Person("Test Person", 41);
+            newPerson.setItems(new ArrayList<>(Collections.singletonList(newItem)));
+            session.save(newPerson);
+            //Delete person's items
+            List<Item> delItems = person.getItems();
+            for (Item singleItem : delItems) {
+                session.remove(singleItem);
             }
-            session.createQuery("delete Person where age > 84").executeUpdate();
+
+            person.getItems().clear();
+
+            //Delete person (check cascade operations)
+            Person person2 = session.get(Person.class, 2);
+            session.remove(person2);
+            //Hibernate cash (refresh)
+            person2.getItems().forEach(t -> t.setOwner(null));
+
+            //Change owner
+            Item item3 = session.get(Item.class, 3);
+            Person person6 = session.get(Person.class, 6);
+            Optional.ofNullable(item3)
+                    .map(Item::getOwner)
+                    .map(Person::getItems)
+                    .ifPresent(i -> i.remove(item3));
+            ;
+            item3.setOwner(person6);
+            person6.getItems().add(item3);
 
             session.getTransaction().commit();
         } finally {
---
+++ src/main/java/org/example/models/Item.java
@@ -0,0 +1,60 @@
+package org.example.models;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "item")
+public class Item {
+
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    int id;
+
+    @Column(name = "name")
+    String itemName;
+
+    @ManyToOne
+    @JoinColumn(name = "person_id", referencedColumnName = "id")
+    Person owner;
+
+    @Override
+    public String toString() {
+        return "Item{" +
+                "itemName='" + itemName + '\'' +
+                ", owner=" + owner +
+                '}';
+    }
+
+    public Item(String itemName, Person owner) {
+        this.itemName = itemName;
+        this.owner = owner;
+    }
+
+    public Item() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getItemName() {
+        return itemName;
+    }
+
+    public void setItemName(String itemName) {
+        this.itemName = itemName;
+    }
+
+    public Person getOwner() {
+        return owner;
+    }
+
+    public void setOwner(Person owner) {
+        this.owner = owner;
+    }
+}
